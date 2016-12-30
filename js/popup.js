239816637e5f9997b3b988e3370dcf6ce5e9541f
Cody Chan <int64ago@gmail.com>
2016-12-30 15:19:19 +0800

    Replace localhost/127.0.0.1 with custom input

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,30 @@
 const QR_URL = 'https://qr.302.at/chart?chs=150x150&cht=qr&choe=UTF-8&chl=';
 
-const setQRCode = url => {
-  document.querySelector('img').setAttribute('src', `${QR_URL}${url}`)
+const imgEle = document.querySelector('img');
+const inputEle = document.querySelector('input');
+
+const getLocalIP = () => localStorage.getItem('qr_url.locale') || '';
+const setLocalIP = (ip) => {
+  localStorage.setItem('qr_url.locale', ip);
+};
+
+const setQRCode = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    let url = tabs[0].url;
+    const localIP = getLocalIP();
+    if (localIP) {
+      url = url.replace(/localhost|127.0.0.1/, localIP);
+    }
+    imgEle.setAttribute('src', `${QR_URL}${url}`)
+  });
 }
 
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  setQRCode(tabs[0].url);
-});
+inputEle.value = getLocalIP();
+
+inputEle.onchange = (event) => {
+  const val = event.target.value;
+  setLocalIP(val);
+  setQRCode();
+}
+
+setQRCode();

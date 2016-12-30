e566fd383822664d083672f8a155d6c9fc2543f9
Cody Chan <int64ago@gmail.com>
2016-12-30 16:47:20 +0800

    Hide replace tips if no local ip

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,8 @@ const QR_URL = 'https://qr.302.at/chart?chs=150x150&cht=qr&choe=UTF-8&chl=';
 
 const imgEle = document.querySelector('img');
 const inputEle = document.querySelector('input');
+const settingEle = document.querySelector('#setting');
+const localEle = document.querySelector('#local');
 
 const getLocalIP = () => localStorage.getItem('qr_url.locale') || '';
 const setLocalIP = (ip) => {
@@ -11,9 +13,14 @@ const setLocalIP = (ip) => {
 const setQRCode = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     let url = tabs[0].url;
+    const reg = /(^https?:\/\/)(localhost|127\.0\.0\.1)/;
     const localIP = getLocalIP();
-    if (localIP) {
-      url = url.replace(/localhost|127.0.0.1/, localIP);
+    if (localIP && reg.test(url)) {
+      settingEle.style.display = 'block';
+      if (/^https?:\/\/localhost/.test(url)) {
+        localEle.innerText = 'localhost';
+      }
+      url = url.replace(reg, `$1${localIP}`);
     }
     imgEle.setAttribute('src', `${QR_URL}${url}`)
   });
